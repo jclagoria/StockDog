5b091d4659d8e8371a50b61eb6d8e50421872c15
jclagoria <Shenkun1979>
2016-12-02 22:53:11 -0300

    Initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name stockDogApp
+ * @description
+ * # stockDogApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('stockDogApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+
+  });
